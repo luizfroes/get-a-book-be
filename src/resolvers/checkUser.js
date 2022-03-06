c4aba4c376b918de89b8fa4c1be24b87328bb812
Luiz Carlos Froes <luizfroes@gmail.com>
2022-03-06 22:11:20 +0000

    Added checkUser.js

---
+++ src/resolvers/checkUser.js
@@ -0,0 +1,13 @@
+const { AuthenticationError } = require("apollo-server");
+
+const { User } = require("../models");
+
+const checkUser = async (_, __, { user }) => {
+  if (user) {
+    const userFromDb = await User.findById(user.id);
+    return userFromDb;
+  }
+  throw new AuthenticationError("Unauthorized access.");
+};
+
+module.exports = checkUser;
